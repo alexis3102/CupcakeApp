25d4610d1dd90f6ace5250ef8fd65329f55435ce
darwinalexis793 <darwinalexiscadavid@gmail.com>
2025-09-23 15:48:58 -0500

    configurar entrypoint de la app

--- app/src/main/java/com/example/cupcakeapp/MainActivity.kt
+++ app/src/main/java/com/example/cupcakeapp/MainActivity.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cupcakeapp.ui.theme.CupcakeAppTheme
 
@@ -19,29 +16,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CupcakeAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                CupcakeApp()
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CupcakeAppTheme {
-        Greeting("Android")
+        CupcakeApp()
     }
 }
